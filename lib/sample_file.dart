25a5e58c5de196bf090d955e3ee21c74e09ac406
VivekViduraj <login.vivekvidhuraj@gmail.com>
2024-07-24 12:17:05 +0530

    second commit

---
+++ lib/sample_file.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SmapleClass extends StatefulWidget {
+  const SmapleClass({super.key});
+
+  @override
+  State<SmapleClass> createState() => _SmapleClassState();
+}
+
+class _SmapleClassState extends State<SmapleClass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
